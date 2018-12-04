4c2de0a6a84b0116a7f8fc5534af225d59986903
charlcater <4677572+charlcater@users.noreply.github.com>
2018-12-04 10:49:24 +0200

    Day 3 part 2

--- day_03/aoc2018_03_1.py
+++ day_03/aoc2018_03_1.py
@@ -1,23 +1,28 @@
 # Advent of Code 2018
-# Day 03: No Matter How You Slice It -- Part 1
+# Day 03: No Matter How You Slice It -- Part 1 & 2
 
 import numpy as np
 import pandas as pd
+
+itercount = 0
+itercount2 = 0
 
 sheet = np.zeros((1000, 1000))
 
 with open('input.txt', 'r') as f:
     data = pd.read_csv('input.txt', delimiter=" ", header=None)
 
-    for index, line in enumerate(f, start=1):
-        id = int(data.iat[index - 1, 0].strip('#'))
+# Part 1
 
-        startpos = ((data.iat[index - 1, 2]).strip(':')).split(',')
+    for index, line in enumerate(f):
+        id = int(data.iat[index, 0].strip('#'))
+
+        startpos = ((data.iat[index, 2]).strip(':')).split(',')
         x = int(startpos[0])
         y = int(startpos[1])
         # print(x, y)
 
-        size = (data.iat[index - 1, 3]).split('x')
+        size = (data.iat[index, 3]).split('x')
         sizex = int(size[0])
         sizey = int(size[1])
         # print(sizex, sizey)
@@ -29,3 +34,28 @@ with open('input.txt', 'r') as f:
 
 overlaps = np.count_nonzero(sheet > 1)  # find all nonzero elements > 1
 print('Number of overlapping squares: {}'.format(overlaps))
+
+
+#  Part 2 (run iteration again, because we need a fully populated sheet to analyse)
+
+with open('input.txt', 'r') as f:
+
+    for index, line in enumerate(f):
+        id = int(data.iat[index, 0].strip('#'))
+
+        startpos = ((data.iat[index, 2]).strip(':')).split(',')
+        x = int(startpos[0])
+        y = int(startpos[1])
+
+        size = (data.iat[index, 3]).split('x')
+        sizex = int(size[0])
+        sizey = int(size[1])
+
+        nsquares = sizex * sizey
+        # print('Size: {}'.format(numsquares))
+
+        region = sheet[x:x + sizex, y:y + sizey]
+
+        if np.count_nonzero(region == 1) == nsquares:
+            print('Non overlapping region found, ID {}'.format(id))
+            break
